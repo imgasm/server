fae5305b8ed48eef85b74337ee9fea69b2f1c78a
maikeme <yd.maike+gh@gmail.com>
2017-08-19 18:45:47 +0200

    improved datastore configuration and error handling

--- database/datastore.go
+++ database/datastore.go
@@ -3,9 +3,9 @@ package database
 import (
 	"cloud.google.com/go/datastore"
 	"context"
+	"fmt"
 	"github.com/imgasm/server/config"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 	"sync"
 )
@@ -16,42 +16,50 @@ var (
 )
 
 func init() {
-	newDatastoreClient()
+	v := viper.New()
+	v.SetConfigName("config")
+	v.AddConfigPath(config.ViperConfigPath)
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error reading config file: %s \n", err))
+	}
+	datastoreClient, err = configureDatastoreClient(v)
+	if err != nil {
+		panic(fmt.Errorf("fatal error configuring datastore client: %s \n", err))
+	}
 }
 
 func newDatastoreClient() {
-	ctx := context.Background()
-	viper.SetConfigName("config")
-	viper.AddConfigPath(config.ViperConfigPath)
-	err := viper.ReadInConfig()
+	runtimeViper := config.RuntimeViper
+	var err error
+	datastoreClient, err = configureDatastoreClient(runtimeViper)
 	if err != nil {
-		// todo: better error handling
-		log.Println("Fatal error config file: %s \n", err)
+		// todo: error handling
 	}
-	env := viper.GetString("environment")
+
+}
+
+func configureDatastoreClient(v *viper.Viper) (*datastore.Client, error) {
+	var projectID string
+
+	env := v.GetString("environment")
 	switch env {
 	case "dev":
-		os.Setenv("DATASTORE_DATASET", viper.GetString("gcloud.datastore.local.dataset"))
-		os.Setenv("DATASTORE_EMULATOR_HOST_PATH", viper.GetString("gcloud.datastore.local.emulator_host_path"))
-		os.Setenv("DATASTORE_HOST", viper.GetString("gcloud.datastore.local.host"))
-		os.Setenv("DATASTORE_EMULATOR_HOST", viper.GetString("gcloud.datastore.local.emulator_host"))
-		os.Setenv("DATASTORE_PROJECT_ID", viper.GetString("gcloud.datastore.local.project_id"))
-		datastoreClient, err = datastore.NewClient(ctx, viper.GetString("gcloud.datastore.local.project_id"))
-		if err != nil {
-			// todo: better error handling
-			log.Println("err creating datastore client:", err)
-		}
+		os.Setenv("DATASTORE_DATASET", v.GetString("gcloud.datastore.local.dataset"))
+		os.Setenv("DATASTORE_EMULATOR_HOST_PATH", v.GetString("gcloud.datastore.local.emulator_host_path"))
+		os.Setenv("DATASTORE_HOST", v.GetString("gcloud.datastore.local.host"))
+		os.Setenv("DATASTORE_EMULATOR_HOST", v.GetString("gcloud.datastore.local.emulator_host"))
+		os.Setenv("DATASTORE_PROJECT_ID", v.GetString("gcloud.datastore.local.project_id"))
+		projectID = v.GetString("gcloud.datastore.local.project_id")
 	case "prod":
-		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", viper.GetString("gcloud.datastore.production.credentials_file"))
-		datastoreClient, err = datastore.NewClient(ctx, viper.GetString("gcloud.datastore.production.project_id"))
-		if err != nil {
-			// todo: better error handling
-			log.Println("err creating datastore client:", err)
-		}
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", v.GetString("gcloud.datastore.production.credentials_file"))
+		projectID = v.GetString("gcloud.datastore.production.project_id")
 	default:
-		// todo: better error handling
-		log.Println("environment not set")
+		// todo: error handling
 	}
+
+	ctx := context.Background()
+	return datastore.NewClient(ctx, projectID)
 }
 
 func DatastoreClient() *datastore.Client {
